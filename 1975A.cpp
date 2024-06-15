e769b1e3d2e05ad4ad2184087584beb825794f24
Rubaya Akter Mim <45107866+rubayamim381@users.noreply.github.com>
2024-06-16 05:24:45 +0600

    div 1+2 A

---
+++ 1975A.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+int a[N];
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i];
+		}
+		int pos = 0;
+		for (int i = 1; i < n; i++) {
+			if (a[i] > a[i + 1]) {
+				pos = i;
+				break;
+			}
+		}
+		if (!pos)
+			cout << "Yes\n";
+		else {
+			int fl = 0;
+			for (int i = pos + 1; i <= n; i++) {
+				int j = (i % n) + 1;
+				if (a[i] > a[j])
+					fl = 1;
+			}
+			if (!fl)
+				cout << "Yes\n";
+			else
+				cout << "No\n";
+		}
+	}
+}
